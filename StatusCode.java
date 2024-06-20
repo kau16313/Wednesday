a9ca11a5ebec9b2e494992ba26175f5405b0f7c7
kau16313 <kau16313@sheridancollege.ca>
2024-06-19 21:18:26 -0400

    Update StatusCode.java

--- StatusCode.java
+++ StatusCode.java
@@ -1,17 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
-package status;
 
-/**
- *
- * @author DELL
- */
-public enum StatusCode {
-    REJECTED,
-    PENDING,
-    PROCESSING,
-    APPROVED
-    
-}
